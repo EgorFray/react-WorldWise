61dbd5912d86f9326070e5a19000d2b14bbd8837
EgorFray <fatamorganaa933@gmail.com>
2025-07-10 21:23:22 +0300

    React project about visited cities and countries. - React router; - Setup with VITE; -useNavigator hook

---
+++ src/components/Map.jsx
@@ -0,0 +1,18 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+
+function Map() {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const navigate = useNavigate();
+
+	const lat = searchParams.get("lat");
+	const lng = searchParams.get("lng");
+
+	return (
+		<div className={styles.mapContainer} onClick={() => navigate("form")}>
+			Map
+		</div>
+	);
+}
+
+export default Map;
